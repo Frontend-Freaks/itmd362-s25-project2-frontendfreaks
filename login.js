9ac2ff4ec398cb916431ffb53e0e775a056f8c4e
amanicps <80164449+AmaniMango@users.noreply.github.com>
2025-04-10 17:18:01 -0500

    toggle button declaring const

--- login.js
+++ login.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggle-create');
+  const loginSection = document.getElementById('login-section');
+  const createSection = document.getElementById('create-section');
 
+});
